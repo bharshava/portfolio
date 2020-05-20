7ba21c324dae5ea6f6e8b28a3007a599bb8b1309
Balanagu Harsha Vardhan <t-bavard@microsoft.com>
2020-05-20 21:09:23 +0530

    Updates

---
+++ precache-manifest.e30c8fe234d3920de27afc0d412d884a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb358ce0e71df6f317c5c387a1ba50c5",
+    "url": "/balanaguharsha/portfolio/index.html"
+  },
+  {
+    "revision": "dd0f0ebe57701b4cff1f",
+    "url": "/balanaguharsha/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3206c9485dc8a530881",
+    "url": "/balanaguharsha/portfolio/static/js/2.65249882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/balanaguharsha/portfolio/static/js/2.65249882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0f0ebe57701b4cff1f",
+    "url": "/balanaguharsha/portfolio/static/js/main.de2f2df6.chunk.js"
+  },
+  {
+    "revision": "dcdcbfa9ee2edd722c8c",
+    "url": "/balanaguharsha/portfolio/static/js/runtime-main.4b8a5136.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/balanaguharsha/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
